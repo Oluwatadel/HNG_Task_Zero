6be6787a9455d176af7b71806cf872a5e12404cc
Oluwatadel <oluwatobiadelesi@gmail.com>
2025-10-17 13:40:37 +0100

    handled

--- obj/Debug/net9.0/HNG_Task_Zero.AssemblyInfo.cs
+++ obj/Debug/net9.0/HNG_Task_Zero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNG_Task_Zero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0e17377bb2291c566ace309b19816044fe7037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5433a224f026e58b76f81d7e6259e880c04cb97c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNG_Task_Zero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HNG_Task_Zero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
